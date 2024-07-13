8d615f8f86346dc8421e552fee7c8f5498f05e67
Zhaozhufeng1 <zhaozhufeng@gmail.com>
2024-07-13 15:01:02 +0000

    updates

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -6,6 +6,7 @@ mkdir -p /workspace/datasets
 sudo chown -R gitpod:gitpod /workspace/opensearch
 sudo  chown -R gitpod:gitpod /workspace/logstash
 ln -s /home/gitpod/logstash-7.13.2 /workspace/logstash/logstash-7.13.2
+sudo chmod +x /workspace/logstash/logstash-7.13.2
 
 
 cd docker
